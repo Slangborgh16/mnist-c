b42f7c313e4e3fafa4c42b6e790f9e3ec39697db
Sam Langborgh <slangborgh16@gmail.com>
2024-02-19 18:17:16 -0500

    Cleaning up main.c to get ready for adding neural net stuff

--- main.c
+++ main.c
@@ -2,32 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mnist.h"
+#include "matrix.h"
 
 
 int main() {
     const char trainingLabelsPath[] = "dataset/train-labels-idx1-ubyte";
     const char trainingImagesPath[] = "dataset/train-images-idx3-ubyte";
-    const char testingLabelsPath[] = "dataset/t10k-labels-idx1-ubyte";
-    const char testingImagesPath[] = "dataset/t10k-images-idx3-ubyte";
 
     LabelData trainingLabels;
     ImageData trainingImages;
-    LabelData testingLabels;
-    ImageData testingImages;
 
     if (loadLabels(trainingLabelsPath, &trainingLabels) == -1) {
         printf("Error reading training labels\n");
         exit(EXIT_FAILURE);
     }
 
-    if (loadLabels(testingLabelsPath, &testingLabels) == -1) {
-        printf("Error reading testing labels\n");
+    if (loadImages(trainingImagesPath, &trainingImages) == -1) {
+        free(trainingLabels.labels);
+        printf("Error loading images\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("There are %u training labels and %u testing labels.\n", trainingLabels.numLabels, testingLabels.numLabels);
-
     free(trainingLabels.labels);
-    free(testingLabels.labels);
+    free(trainingImages.pixelData);
     exit(EXIT_SUCCESS);
 }
