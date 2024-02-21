d7dbd3e7bec62049f466c5122a6bb69a56ca282c
Sam Langborgh <slangborgh16@gmail.com>
2024-02-20 20:08:46 -0500

    Implemented forward propagation and made some fixes

--- include/neuralnet.h
+++ include/neuralnet.h
@@ -4,29 +4,28 @@
 #include <math.h>
 
 typedef struct Network {
-    int numInputNodes;
-    int numHiddenNodes;
-    int numOutputNodes;
+    int inputNodes;
+    int hiddenNodes;
+    int outputNodes;
 
-    double inputLayer[numInputNodes];
-    double hiddenLayer[numHiddenNodes];
-    double outputLayer[numOutputNodes];
+    double inputLayer[inputNodes];
+    double hiddenLayer[hiddenNodes];
+    double outputLayer[outputNodes];
 
-    double weights1[numHiddenNodes][numInputNodes];
-    double weights2[numOutputNodes][numHiddenNodes];
+    double weights1[hiddenNodes][inputNodes];
+    double weights2[outputNodes][hiddenNodes];
 
-    double bias1[numHiddenNodes];
-    double bias2[numOutputNodes];
+    double bias1[hiddenNodes];
+    double bias2[outputNodes];
 } Network;
 
 int vecAdd(int cols, double a[cols], double b[cols], double output[cols]);
 int vecSubtract(int cols, double a[cols], double b[cols], double output[cols]);
-int matrixNormalize(int rows, double input[rows], double output[rows], double maximum);
+int vecNormalize(int cols, double input[cols], double output[cols], double maximum);
 int initializeWeights(int rows, int cols, double input[rows][cols]);
 int dotProduct(int rows, int cols, double matrix[rows][cols], double vec[cols], double output[rows]);
-int relu(int rows, double input[rows], double output[rows]);
-int softmax(int rows, double input[rows], double output[rows]);
-int forwardprop(int rows, int cols, double weights[rows][cols], double vec[cols], \
-        double bias[rows], double output[rows]);
+int relu(int cols, double input[cols], double output[cols]);
+int softmax(int cols, double input[cols], double output[cols]);
+int forwardprop(Network* nnet) {
 
 #endif
--- src/neuralnet.c
+++ src/neuralnet.c
@@ -16,8 +16,8 @@ int vecSubtract(int cols, double a[cols], double b[cols], double output[cols]) {
 }
 
 
-int matrixNormalize(int rows, double input[rows], double output[rows], double maximum) {
-    for (int i = 0; i < rows; i++) {
+int vecNormalize(int cols, double input[cols], double output[cols], double maximum) {
+    for (int i = 0; i < cols; i++) {
         output[i] = input[i] / maximum;
     }
     return 0;
@@ -47,33 +47,51 @@ int dotProduct(int rows, int cols, double matrix[rows][cols], \
 }
 
 
-
-int relu(int rows, double input[rows], double output[rows]) {
-    for (int i = 0; i < rows; i++)
+int relu(int cols, double input[cols], double output[cols]) {
+    for (int i = 0; i < cols; i++)
         output[i] = fmax(0.00f, input[i]);
 
     return 0;
 }
 
 
-int softmax(int rows, double input[rows], double output[rows]) {
+int softmax(int cols, double input[cols], double output[cols]) {
     double summation = 0.00f;
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < cols; i++) {
         double val = powf(M_E, input[i]);
         summation += val;
         output[i] = val;
     }
 
-    for (int i = 0; i < rows; i++) {
+    for (int i = 0; i < cols; i++) {
         output[i] /= summation;
     }
 
     return 0;
 }
 
-int forwardprop(int rows, int cols, double weights[rows][cols], double vec[cols], \
-        double bias[rows], double output[rows]) {
-    dotProduct(rows, cols, weights, vec, output);
-    vecAdd(cols, output, bias, output);
+
+int forwardprop(Network* nnet) {
+    int iNodes = nnet->inputNodes;
+    int hNodes = nnet->hiddenNodes;
+    int oNodes = nnet->outputNodes;
+
+    double* iLayer = nnet->inputLayer;
+    double* hLayer = nnet->hiddenLayer;
+    double* oLayer = nnet->outputLayer;
+
+    double* w1 = nnet->weights1;
+    double* w2 = nnet->weights2;
+
+    double* b1 = nnet->bias1;
+    double* b2 = nnet->bias2;
+
+    dotProduct(hNodes, iNodes, w1, iLayer, hLayer);
+    vecAdd(hNodes, hLayer, b1, hLayer);
+    reul(hNodes, hLayer, hLayer);
+    dotProduct(oNodes, hNodes, w2, hLayer, oLayer);
+    vecAdd(oNodes, oLayer, b2, oLayer);
+    softmax(oNodes, oLayer, oLayer);
+
     return 0;
 }
